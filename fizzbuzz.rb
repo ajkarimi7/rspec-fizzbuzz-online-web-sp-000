5b160fce13043e0b943107fea12ffd4a07487868
Ali Karimi <ajkarimi7@gmail.com>
2019-08-14 21:19:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,22 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  if int % 3 == 0
+def fizzbuzz(int)
+  
+  if int % 15 == 0
+    "Fizzbuzz"
+  end 
+  
+  elsif int % 3 == 0 
     "Fizz"
   end 
-elsif int % 5 == 0 
-  ""
+  
+  elsif int % 5 == 0
+    "Buzz"
+  end 
+  
+  else
+    "nil"
+  end 
+  
 end 
